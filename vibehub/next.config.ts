9b9378d1f56cd1052f5c772ec243a2f1e8aebb94
shefket-mustafa <miracle_1@abv.bg>
2025-08-31 12:47:14 +0300

    Endpoint added for fetching all users and a contacts section created on the left feed section

--- vibehub/next.config.ts
+++ vibehub/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {domains: ["picsum.photos", "images.unsplash.com", "searchengineland.com", "mma.prnewswire.com", "miro.medium.com", "www.zilliondesigns.com"]}
 };
 
 export default nextConfig;
